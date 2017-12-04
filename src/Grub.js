e00d22205e9b1d84b90e1ace8ebf819a79498d2a
justin <justindulark@gmail.com>
2017-12-04 12:06:17 -0800

    Revert "Added prototype for Bee class"
    This reverts commit e9a38324ea77abbe1dcab2aee04a3e94a7cc1811.

--- src/Grub.js
+++ src/Grub.js
@@ -1,7 +1,7 @@
 var Grub = function() {
-  this.age = 0;
-  this.color = 'pink';
-  this.food = 'jelly';
+	this.age = 0;
+	this.color = 'pink';
+	this.food = 'jelly';
+	this.eat = function();
 };
 
-Grub.prototype.eat = function(){};
